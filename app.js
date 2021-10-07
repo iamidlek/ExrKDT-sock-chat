c021d954bbd03bbcaf295efd0a45172b2830758d
iamidlek <iamidlek@naver.com>
2021-10-07 17:54:17 +0900

    feat: Static file serve (express done)
    with adding app.use('public', express.static...

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const path = require('path');
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
+// static 파일 들을 담아두는 경로 지정
+app.use("/public", express.static(__dirname + "/public"));
+
 const indexRouter = require('./routes/index');
 const clubsRouter = require('./routes/clubs')
 
