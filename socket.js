771e479fc1628be9e5dbc0cede2e8b78799c966a
iamidlek <iamidlek@naver.com>
2021-10-07 18:53:58 +0900

    Done! solo chat

--- socket.js
+++ socket.js
@@ -5,6 +5,7 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('A user is now connected')
     socket.on('chat message', (msg) => {
+      io.emit('chat message', msg)
       console.log("message: ", msg)
     })
     socket.on('disconnect', () => {
