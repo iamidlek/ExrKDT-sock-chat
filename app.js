a035cf6f53768ae6ebc9b603efacfe1040f34326
iamidlek <iamidlek@naver.com>
2021-10-07 16:36:28 +0900

    feat: Add indexRouter
    split router and main app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Express work!');
-});
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
 
 server.listen(3000, () => {
   console.log('Express Server is now listening on localhost:3000..')
-})
+});
