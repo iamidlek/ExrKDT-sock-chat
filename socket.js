ffc903dd4b219814cb1aa54e754b487fced1f208
iamidlek <iamidlek@naver.com>
2021-10-07 18:42:41 +0900

    feat: emit message
    console.lgo emitted message

--- socket.js
+++ socket.js
@@ -4,5 +4,11 @@ const { Socket } = require("socket.io")
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('A user is now connected')
+    socket.on('chat message', (msg) => {
+      console.log("message: ", msg)
+    })
+    socket.on('disconnect', () => {
+      console.log('disconnected')
+    })
   })
 }
